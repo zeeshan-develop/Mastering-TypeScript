27f183246d75c3443f9131d758d155e2e96e8f13
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2024-11-06 01:43:53 +0500

    default_optional_params

---
+++ function/default_optional_params.js
@@ -0,0 +1,12 @@
+"use strict";
+// default &  optional parameters
+// default parameter
+const Fun = (degree, rollno = 64) => {
+    return `i am studying in ${degree} & my roll no is ${rollno}`;
+};
+console.log(Fun("BSSE"));
+// optional parameter
+const Fun2 = (degree, rollno) => {
+    return `i am studying in ${degree}`;
+};
+console.log(Fun("BSSE"));
---
+++ function/default_optional_params.ts
@@ -0,0 +1,13 @@
+// default &  optional parameters
+
+// default parameter
+const Fun = (degree: string, rollno: number = 64): string => {
+  return `i am studying in ${degree} & my roll no is ${rollno}`;
+};
+console.log(Fun("BSSE"));
+
+// optional parameter
+const Fun2 = (degree: string, rollno?: number): string => {
+  return `i am studying in ${degree}`;
+};
+console.log(Fun("BSSE"));
