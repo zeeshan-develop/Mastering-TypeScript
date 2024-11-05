b89e27e14656add277a20daa25c9e94e3b6a1bdc
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2024-11-05 22:21:52 +0500

    Deep Dive into TS Code, Catching Errors & Solving & TS Config File

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("I am learning TypecSript");
+var age = 21;
+age = 34;
+console.log(age);
+// TODO 1: Write a program to add two numbers?
+// Todo 2: How to catch errors & solve it?
+// Todo 3: Ts configuration File
+// * func define
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(2, 3));
---
+++ index.ts
@@ -0,0 +1,15 @@
+console.log("I am learning TypecSript");
+var age: number = 21;
+age = 34;
+console.log(age);
+
+// TODO 1: Write a program to add two numbers?
+// Todo 2: How to catch errors & solve it?
+// Todo 3: Ts configuration File
+
+// * func define
+function addNumbers(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(addNumbers(2, 3));
